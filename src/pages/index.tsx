f428b3f38e5c64b85d8c2537e205b88f0c664f3f
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2023-01-08 09:20:00 -0300

    Add getServerSideProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
+import { GetServerSideProps } from 'next';
 import { TemplateLogin } from 'template/TemplateLogin';
 
 export default function Login() {
   return <TemplateLogin />;
 }
+
+export const getServerSideProps: GetServerSideProps = (context) => {
+  return {
+    props: {},
+  };
+};
